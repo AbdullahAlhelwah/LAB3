eeaf625910c2c65e944f737f12d798cfcd8e257d
Mohammed <mohammed@Mohammeds-MacBook-Pro.local>
2023-02-13 23:30:55 +0300

    huh

--- sorter.java
+++ sorter.java
@@ -22,4 +22,21 @@ public class sorter{
             arr[i] = temp;
         }
     }
+    public static void insertionSort(int array[]) {
+        int size = array.length;
+
+        for (int step = 1; step < size; step++) {
+            int key = array[step];
+            int j = step - 1;
+
+            
+            while (j >= 0 && key < array[j]) {
+                array[j + 1] = array[j];
+                --j;
+            }
+
+            
+            array[j + 1] = key;
+        }
+    }
 }
